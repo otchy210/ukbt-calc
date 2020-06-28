688de2cd5bf1472e4745436f403e0e7ca713e074
Otchy <id+github@indeed.com>
2020-06-28 22:48:56 +0900

    Handle mode

---
+++ src/js/components/Calculator.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import ResultCard from './ResultCard';
+
+const Calculator = (props) => {
+    const {loading, type, buffs, data} = props;
+
+    const [selectedTab, setSelectedTab] = useState('calc');
+    const [selectedCell, setSelectedCell] = useState('unselected');
+    const [lap, setLap] = useState(undefined);
+    const [latestResult, setLatestResult] = useState(undefined);
+
+    const {a, b, cells} = data;
+    const cellOptions = [];
+    const cellsMap = {unselected: {lv: '', cont: '', area: ''}};
+    cells.filter(cell => cell.type === type).sort((l, r) => {
+        if (l.x !== r.x) {
+            return l.x - r.x;
+        }
+        return l.y - r.y;
+    }).forEach(cell => {
+        cellsMap[cell.key] = cell;
+        cellOptions.push(<option value={cell.key}>{cell.key}</option>);
+    });
+
+    const selectCell = (e) => {
+        setSelectedCell(e.target.value);
+    };
+    const changeLap = (e) => {
+        setLap(e.target.value);
+    }
+    const calc = (e) => {
+        e.preventDefault();
+        if (selectedCell === 'unselected') {
+            return;
+        }
+        if (!lap) {
+            return;
+        }
+        const cell = cellsMap[selectedCell];
+        const {lv} = cell;
+        const dropRate = a * lv + b;
+        const enemies = 60 * 60 / lap * 5;
+        const base = dropRate * enemies;
+        const result = {...cell, lap, base, ts: (new Date()).getTime()};
+        setLatestResult(result);
+    };
+
+    return <React.Fragment>
+    <div class="row tab">
+        <div class="col-sm-6 input-group fluid">
+            <button
+                class={selectedTab === 'calc' ? 'primary' : ''}
+                onClick={() => setSelectedTab('calc')}
+                disabled={loading}
+            >計算機</button>
+        </div>
+        <div class="col-sm-6 input-group fluid">
+            <button
+                class={selectedTab === 'history' ? 'primary' : ''}
+                onClick={() => setSelectedTab('history')}
+                disabled={loading}
+            >計算結果履歴</button>
+        </div>
+    </div>
+    {selectedTab === 'calc' && <form>
+        <div class="row">
+            <div class="col-sm-6 col-md-4 input-group fluid">
+                <label for="cell">放置マス</label>
+                <select id="cell" onChange={selectCell} disabled={loading}>
+                    <option value="unselected">選択</option>
+                    {cellOptions}
+                </select>
+            </div>
+            <div class="col-sm-6 col-md-4 input-group fluid">
+                <label for="lap">ラップ</label>
+                <input type="number" id="lap" value={lap} step="0.01" min="0" max="60" onChange={changeLap} disabled={loading} placeholder="数値入力" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-4 input-group fluid">
+                <label for="lv">敵 Lv</label>
+                <input id="lv" value={cellsMap[selectedCell].lv} disabled={true} />
+            </div>
+            <div class="col-sm-6 col-md-4 input-group fluid">
+                <label for="cont">大陸</label>
+                <input id="cont" value={cellsMap[selectedCell].cont} disabled={true} />
+            </div>
+            <div class="col-sm-12 col-md-4 input-group fluid">
+                <label for="area">地域</label>
+                <input id="area" value={cellsMap[selectedCell].area} disabled={true} />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 input-group fluid">
+                <button class="tertiary" onClick={calc} disabled={loading}>予測ドロップ数を計算</button>
+            </div>
+        </div>
+    </form>}
+    {selectedTab === 'calc' && latestResult && <div class="row">
+        <div class="col-sm-12 col-md-4">
+            <ResultCard buffs={buffs} result={latestResult} />
+        </div>
+    </div>}
+    {selectedTab === 'history' && <div class="row">
+        <div class="col-sm-12">
+            ごめんまだです！過去の計算結果の履歴が見れて、結果を比較できる…ようになる予定。乞うご期待！
+        </div>
+    </div>}
+    </React.Fragment>
+}
+
+export default Calculator;
--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import ResultCard from './ResultCard';
+import classNames from 'classnames';
+import Calculator from './Calculator';
 import { setLocal, getLocal } from '../common';
 
 const Form = (props) => {
@@ -20,10 +21,6 @@ const Form = (props) => {
         setLocal('buffs', updatedBuffs);
         setBuffs(updatedBuffs);
     };
-    const [selectedTab, setSelectedTab] = useState('calc');
-    const [selectedCell, setSelectedCell] = useState('unselected');
-    const [lap, setLap] = useState(undefined);
-    const [latestResult, setLatestResult] = useState(undefined);
 
     const mBuffOptions = [];
     const formatPercent = (lv) => {
@@ -49,50 +46,13 @@ const Form = (props) => {
         setLoading(false);
     }, []);
 
-    const {data} = props;
-    const {a, b, cells} = data;
-    const cellOptions = [];
-    const cellsMap = {unselected: {lv: '', cont: '', area: ''}};
-    cells.filter(cell => cell.type === 'Y').sort((l, r) => {
-        if (l.x !== r.x) {
-            return l.x - r.x;
-        }
-        return l.y - r.y;
-    }).forEach(cell => {
-        cellsMap[cell.key] = cell;
-        cellOptions.push(<option value={cell.key}>{cell.key}</option>);
-    });
-
-    const selectCell = (e) => {
-        setSelectedCell(e.target.value);
-    };
-    const changeLap = (e) => {
-        setLap(e.target.value);
-    }
-    const calc = (e) => {
-        e.preventDefault();
-        if (selectedCell === 'unselected') {
-            return;
-        }
-        if (!lap) {
-            return;
-        }
-        const cell = cellsMap[selectedCell];
-        const {lv} = cell;
-        const dropRate = a * lv + b;
-        const enemies = 60 * 60 / lap * 5;
-        const base = dropRate * enemies;
-        const result = {...cell, lap, base, ts: (new Date()).getTime()};
-        setLatestResult(result);
-    };
-
     return <React.Fragment>
     <div class="container">
         <form>
             <div class="row">
                 <div class="col-sm-12 col-md-4 input-group fluid">
                     <label for="type">モード</label>
-                    <select id="type" value={mode} onChange={(e) => setMode(e.target.value)} disabled={loading}>
+                    <select id="type" class={classNames('mode', mode)} value={mode} onChange={(e) => setMode(e.target.value)} disabled={loading}>
                         <option value="Y">強化書</option>
                         <option value="R">血石</option>
                         <option value="B">魔法石</option>
@@ -114,64 +74,19 @@ const Form = (props) => {
                 </div>
             </div>
         </form>
-        <div class="row tab">
-            <div class="col-sm-6 input-group fluid">
-                <button
-                    class={selectedTab === 'calc' ? 'primary' : ''}
-                    onClick={() => setSelectedTab('calc')}
-                    disabled={loading}
-                >計算機</button>
-            </div>
-            <div class="col-sm-6 input-group fluid">
-                <button
-                    class={selectedTab === 'history' ? 'primary' : ''}
-                    onClick={() => setSelectedTab('history')}
-                    disabled={loading}
-                >計算結果履歴</button>
-            </div>
-        </div>
-        {selectedTab === 'calc' && <form>
-            <div class="row">
-                <div class="col-sm-12 col-md-4 input-group fluid">
-                    <label for="cell">放置マス</label>
-                    <select id="cell" onChange={selectCell} disabled={loading}>
-                        <option value="unselected">選択して下さい</option>
-                        {cellOptions}
-                    </select>
+        {mode !== 'P' && mode === 'Y' && <Calculator loading={loading} type={mode} buffs={buffs} data={props.data} />}
+        {mode !== 'P' && mode !== 'Y' && <div class="row">
+            <div class="col-sm-12">
+                <div class="card fluid">
+                    データ不足でまだ使えません＞＜
                 </div>
-                <div class="col-sm-12 col-md-4 input-group fluid">
-                    <label for="lap">ラップ</label>
-                    <input type="number" id="lap" value={lap} step="0.01" min="0" max="60" onChange={changeLap} disabled={loading} placeholder="ラップタイムを入力" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-12 col-md-4 input-group fluid">
-                    <label for="lv">敵 Lv</label>
-                    <input id="lv" value={cellsMap[selectedCell].lv} disabled={true} />
-                </div>
-                <div class="col-sm-12 col-md-4 input-group fluid">
-                    <label for="cont">大陸</label>
-                    <input id="cont" value={cellsMap[selectedCell].cont} disabled={true} />
-                </div>
-                <div class="col-sm-12 col-md-4 input-group fluid">
-                    <label for="area">地域</label>
-                    <input id="area" value={cellsMap[selectedCell].area} disabled={true} />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-12 input-group fluid">
-                    <button class="tertiary" onClick={calc} disabled={loading}>予測ドロップ数を計算</button>
-                </div>
-            </div>
-        </form>}
-        {selectedTab === 'calc' && latestResult && <div class="row">
-            <div class="col-sm-12 col-md-4">
-                <ResultCard buffs={buffs} result={latestResult} />
             </div>
         </div>}
-        {selectedTab === 'history' && <div class="row">
+        {mode === 'P' && <div class="row">
             <div class="col-sm-12">
-                ごめんまだです！過去の計算結果の履歴が見れて、結果を比較できる…ようになる予定。乞うご期待！
+                <div class="card fluid">
+                    ここから報告できるようにします。
+                </div>
             </div>
         </div>}
     </div>
